df54d9d53f3e7b76fd7d9050cf440be937ecb56b
jilen <jilen.zhang@gmail.com>
2012-12-05 18:57:33 +0800

    add more from learn you a haskell for great good

--- Monads.hs
+++ Monads.hs
@@ -12,3 +12,15 @@ fortune = do
   where
     tellFortune = "fortune is kind of you"
 
+--example from learn you a haskell for great good
+
+type Birds = Int
+type Pole = (Birds, Birds)
+
+landLeft :: Birds -> Pole -> Pole
+landLeft n (left, right) = (left + n, right)
+
+landRight :: Birds -> Pole -> Pole
+landRight n (left, right) = (left, right + n )
+
+x -: f = f x 
